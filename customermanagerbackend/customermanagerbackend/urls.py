ff3e29d2f38abac52ebe0ef60d26aa5c58b421e0
zied.bejaoui <zied.bejaoui@kyriba.com>
2021-11-24 21:53:13 +0100

    coomit

---
+++ customermanagerbackend/customermanagerbackend/urls.py
@@ -0,0 +1,42 @@
+"""customermanagerbackend URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from customers import views
+from django.conf.urls import url
+from django.urls import include, path, re_path
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.contrib import admin
+from django.conf import settings
+import os.path
+
+from wagtail.core import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('cms/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('pages/', include(wagtail_urls)),
+    url(r'^api/customers/$', views.customers_list),
+    url(r'api/customers/(?P<pk>[0-9]+)$', views.customers_detail),
+]
+
